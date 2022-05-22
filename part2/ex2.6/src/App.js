4f91ff83bee661a82ef1f5eb9e2c5eb2a3202574
Daniel Scislowski <mr.scislowski@gmail.com>
2022-05-22 16:13:44 -0500

    implement The Phonebook Step4 (2.9)

--- part2/ex2.6/src/App.js
+++ part2/ex2.6/src/App.js
@@ -14,10 +14,15 @@ const Entry = ({person}) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterString, setFilterString] = useState('')
 
   const handleInputChange = (changeFn) => {
     const changeHandler = (event) => {
@@ -35,12 +40,22 @@ const App = () => {
     }
     setNewName('')
     setNewNumber('')
-    
   }
+
+  const personsToShow = persons.filter(
+    person => person.name.toLowerCase().includes(filterString.toLowerCase()))
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with 
+        <input
+        value={filterString}
+        onChange={handleInputChange(setFilterString)}
+        />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addNewEntry}>
         <div>
           name: 
@@ -61,7 +76,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Entries persons={persons}/>
+      <Entries persons={personsToShow}/>
     </div>
   )
 }
