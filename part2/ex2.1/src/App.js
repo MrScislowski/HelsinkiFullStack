5318952e984f7c409bc827b3d8251af638ebd15f
Daniel Scislowski <mr.scislowski@gmail.com>
2022-05-21 20:39:23 -0500

    implement separate module (2.5)

--- part2/ex2.1/src/App.js
+++ part2/ex2.1/src/App.js
@@ -1,45 +1,4 @@
-
-const ExerciseSum = ({course}) => {
-  const total = course.parts.reduce((sum, part) => sum + part.exercises, 0)
-  return (
-    <strong>
-      total of {total} exercises
-    </strong>
-  )
-}
-
-const Course = ({course}) => {
-  return (
-    <>
-    <Header name={course.name} />
-    <PartCollection parts={course.parts} />
-    <ExerciseSum course={course} />
-    </>
-  )
-}
-
-const Header = ({name}) => {
-  return (
-  <h1> {name} </h1>
-  )
-}
-
-const PartCollection = ({parts}) => {
-  return (
-    <div>
-    {parts.map(part => <Part key={part.id} part={part} />)}
-    </div>
-  )
-}
-
-const Part = ({part}) => {
-  const {name, exercises} = part
-  return (
-    <p>
-      {name} {exercises}
-    </p>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
