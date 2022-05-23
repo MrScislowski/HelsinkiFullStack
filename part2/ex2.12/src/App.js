e37441674866c4ba31c76874ef25abdf09d531bd
Daniel Scislowski <mr.scislowski@gmail.com>
2022-05-23 14:01:19 -0500

    implement Data for countries, step1 (2.12)

---
+++ part2/ex2.12/src/App.js
@@ -0,0 +1,82 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
+const CountryInput = ({countrySearchKey, setCountrySearchKey}) => {
+  return (
+    <p>
+      find countries 
+      <input value={countrySearchKey}
+        onChange={(e) => setCountrySearchKey(e.target.value)}/>
+    </p>
+  )
+}
+
+const DisplayCountrySearchInfo = ({countries}) => {
+  if (countries.length >= 10) {
+    return "Too many matches, specify another filter"
+  }
+  else if (countries.length === 1) {
+    return <DisplayCountryStats country={countries[0]} />
+  } else {
+    return <DisplayAbbreviatedResults countries={countries} />
+  }
+}
+  
+const DisplayAbbreviatedResults = ({countries}) => {
+  return (
+    <>
+    {countries.map(c => <p key={c.ccn3}>{c.name.common}</p>)}
+    </>
+  )
+}
+
+const DisplayCountryStats = ({country}) => {
+  
+  return (
+    <div>
+      <h2>{country.name.common}</h2>
+      <p>capital {country.capital}</p>
+      <p>area {country.area}</p>
+
+      <h3>languages: </h3>
+      <ul>
+      {Object.keys(country.languages).map((k, i) => {
+        return <li key={i}>{country.languages[k]}</li>
+      })}
+      </ul>
+      <img src={country.flags.png} alt="country flag"/>
+    </div>
+  )
+
+
+}
+
+
+
+const App = () => {
+  const [countrySearchKey, setCountrySearchKey] = useState('')
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        setCountries(response.data)
+      })
+  }, [])
+
+  const countriesToDisplay = countries.filter(country => {
+    return country.name.common.toLowerCase().includes(countrySearchKey.toLowerCase())
+  })
+
+
+  return (
+    <div>
+    <CountryInput countrySearchKey={countrySearchKey} setCountrySearchKey={setCountrySearchKey} />
+    <DisplayCountrySearchInfo countries={countriesToDisplay} />
+    </div>
+  );
+}
+
+export default App;
