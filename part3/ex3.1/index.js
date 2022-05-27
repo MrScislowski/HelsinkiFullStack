52700dc079c1b05294bd00451e651ae93160b770
Daniel Scislowski <mr.scislowski@gmail.com>
2022-05-27 15:35:17 -0500

    implement Phonebook backend step8 (3.8)

--- part3/ex3.1/index.js
+++ part3/ex3.1/index.js
@@ -4,7 +4,25 @@ const morgan = require('morgan')
 
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+const customMorgan = (tokens, req, res) => {
+    const postBody = (tokens.method(req, res) === "POST")
+        ? JSON.stringify(req.body)
+        : ""
+    
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), 
+        '-',
+        tokens["response-time"](req, res),
+        'ms',
+        postBody
+    ].join(' ')
+}
+
+app.use(morgan(customMorgan))
 
 let persons = [
     { 
