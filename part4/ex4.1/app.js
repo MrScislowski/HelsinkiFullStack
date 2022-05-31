982ed99433b808ec6dd5f65a91344568af38a8ab
Daniel Scislowski <mr.scislowski@gmail.com>
2022-05-31 06:36:44 -0500

    add express router

--- part4/ex4.1/app.js
+++ part4/ex4.1/app.js
@@ -6,26 +6,6 @@ const Blog = require('./models/blog')
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-app.get('/debug', (request, response) => {
-  response.json({message: 'hello'})
-})
+app.use('/api/blogs', require('./controllers/blogs'))
 
 module.exports = app
