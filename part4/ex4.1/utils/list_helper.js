704fc18918490ecdc01d93e5229c5869085fed2d
Daniel Scislowski <mr.scislowski@gmail.com>
2022-06-01 19:49:16 -0500

    implement 4.5*: helper functions and unit tests, step3

--- part4/ex4.1/utils/list_helper.js
+++ part4/ex4.1/utils/list_helper.js
@@ -7,6 +7,17 @@ const totalLikes = (blogs) => {
     0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((curFave, blog) => {
+    if (curFave.likes < blog.likes) {
+      const newFave = {title: blog.title, author: blog.author, likes: blog.likes}
+      return newFave
+    } 
+    return curFave
+  },
+  {title: blogs[0].title, author: blogs[0].author, likes: blogs[0].likes})
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
