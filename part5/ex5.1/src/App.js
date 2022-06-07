a8d479c6ae837205b042a59feb86561295e48fc6
Daniel Scislowski <mr.scislowski@gmail.com>
2022-06-07 07:10:01 -0500

    set up bloglist and controlled component login

--- part5/ex5.1/src/App.js
+++ part5/ex5.1/src/App.js
@@ -4,6 +4,9 @@ import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +14,44 @@ const App = () => {
     )  
   }, [])
 
-  return (
-    <div>
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    setUser(username)
+  }
+
+  const loginForm = () => (
+      <>
+      <h2> log in to application</h2>
+      <form onSubmit={handleLogin}>
+      username <input type="text" value={username} onChange={({target}) => setUsername(target.value)} /> <br/>
+      password <input type="password" value={password} onChange={({target}) => setPassword(target.value)} /> <br />
+      <button type="submit"> login </button>
+      </form>
+      </>
+    )
+
+  const blogsDisplay = () => (
+      <>
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+      </>
+    )
+
+  const loginStatusDisplay = () => (
+    <p>{user} logged in</p>
+  )
+
+  return (
+    <div>
+      {user === null?
+      loginForm() :
+      <>
+      {loginStatusDisplay()}
+      {blogsDisplay()}
+      </>
+      }
     </div>
   )
 }
