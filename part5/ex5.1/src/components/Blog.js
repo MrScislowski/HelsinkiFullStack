0c646cc9979deb3d1cea166ed832becbdecc341a
Daniel Scislowski <mr.scislowski@gmail.com>
2022-06-10 15:18:24 -0500

    5.7* Blog list frontend, step7

--- part5/ex5.1/src/components/Blog.js
+++ part5/ex5.1/src/components/Blog.js
@@ -1,7 +1,36 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [detailsShown, setDetailsShown] = useState(false)
+
+  const toggleVisibility = () => {
+    setDetailsShown(!detailsShown)
+  }
+
+  const showWhenVisible = { display: detailsShown ? '' : 'none' }
+
+  const buttonText = (detailsShown) ? 'hide' : 'view'
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author} <button onClick={toggleVisibility}>{buttonText}</button>
+      </div>
+      <div style={showWhenVisible}>
+        {blog.url} <br/>
+        likes {blog.likes} <button>like</button> <br />
+        {(blog.user) ? blog.user.name: ''}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
