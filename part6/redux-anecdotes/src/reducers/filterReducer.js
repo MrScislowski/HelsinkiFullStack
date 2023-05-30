6989abbe34b7786edd59c33efb5a88dcb27a8e98
Daniel Scislowski <mr.scislowski@gmail.com>
2023-05-29 20:47:51 -0500

    6.11 Better anecdotes, step9 - createslice for filter too

--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,21 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit"
+
 const initialState = ''
 
-const filterReducer = (state = initialState, action) => {
 
-  switch (action.type) {
-    case 'UPDATE_FILTER':
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    updateFilter(state, action) {
       return action.payload
-    default:
-      return state
+    }
   }
-}
+})
 
-export const updateFilter = (newFilterText) => {
-  return {
-    type: 'UPDATE_FILTER',
-    payload: newFilterText
-  }
-}
-
-
-export default filterReducer
+export const { updateFilter } = filterSlice.actions
+export default filterSlice.reducer
