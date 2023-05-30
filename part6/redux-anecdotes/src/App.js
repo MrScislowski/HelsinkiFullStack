88f9ae169740835eab4dc978443a67bda9005d3d
Daniel Scislowski <mr.scislowski@gmail.com>
2023-05-30 07:59:41 -0500

    6.14 step1 - get anecdotes from json-server

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,10 +1,23 @@
 import { useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import anecdoteService from './services/anecdotes'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    anecdoteService.getAnecdotesFromDb().then(anecdotes => {
+      dispatch(setAnecdotes(anecdotes))
+    })
+  }, [dispatch])
+
   const anecdotes = useSelector(({anecdotes, filter}) => {
     if ( filter === '' ) {
       return anecdotes
