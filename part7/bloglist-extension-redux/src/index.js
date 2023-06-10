2b58d7bbff6e7dcbaf1f5114beadf6294a57dd5d
Daniel Scislowski <mr.scislowski@gmail.com>
2023-06-10 08:12:06 -0500

    use redux-toolkit instead of vanilla

--- part7/bloglist-extension-redux/src/index.js
+++ part7/bloglist-extension-redux/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import notificationReducer from "./reducers/notificationReducer";
+import { configureStore } from "@reduxjs/toolkit"
 import { createStore } from "redux"
 import {Provider} from "react-redux"
 
 
-const store = createStore(notificationReducer);
+const store = configureStore({
+    reducer: notificationReducer
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store} >
