094d6250b40f21804f8a3cd1fd237c28cb456000
Daniel Scislowski <mr.scislowski@gmail.com>
2023-06-30 13:42:24 -0500

    implement allAuthors resolver

--- part8/ex8.1/index.js
+++ part8/ex8.1/index.js
@@ -19,84 +19,6 @@ mongoose
     console.log("error connection to MongoDB:", error.message);
   });
 
-// let authors = [
-//   {
-//     name: "Robert Martin",
-//     id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-//     born: 1952,
-//   },
-//   {
-//     name: "Martin Fowler",
-//     id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-//     born: 1963,
-//   },
-//   {
-//     name: "Fyodor Dostoevsky",
-//     id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-//     born: 1821,
-//   },
-//   {
-//     name: "Joshua Kerievsky", // birthyear not known
-//     id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-//   },
-//   {
-//     name: "Sandi Metz", // birthyear not known
-//     id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-//   },
-// ];
-
-// let books = [
-//   {
-//     title: "Clean Code",
-//     published: 2008,
-//     author: "Robert Martin",
-//     id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-//     genres: ["refactoring"],
-//   },
-//   {
-//     title: "Agile software development",
-//     published: 2002,
-//     author: "Robert Martin",
-//     id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-//     genres: ["agile", "patterns", "design"],
-//   },
-//   {
-//     title: "Refactoring, edition 2",
-//     published: 2018,
-//     author: "Martin Fowler",
-//     id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-//     genres: ["refactoring"],
-//   },
-//   {
-//     title: "Refactoring to patterns",
-//     published: 2008,
-//     author: "Joshua Kerievsky",
-//     id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-//     genres: ["refactoring", "patterns"],
-//   },
-//   {
-//     title: "Practical Object-Oriented Design, An Agile Primer Using Ruby",
-//     published: 2012,
-//     author: "Sandi Metz",
-//     id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-//     genres: ["refactoring", "design"],
-//   },
-//   {
-//     title: "Crime and punishment",
-//     published: 1866,
-//     author: "Fyodor Dostoevsky",
-//     id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-//     genres: ["classic", "crime"],
-//   },
-//   {
-//     title: "The Demon ",
-//     published: 1872,
-//     author: "Fyodor Dostoevsky",
-//     id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-//     genres: ["classic", "revolution"],
-//   },
-// ];
-
 const typeDefs = `
 type Author {
 name: String!
@@ -130,28 +52,29 @@ type Book {
 const resolvers = {
   Query: {
     bookCount: async () => {
-      const allBooks = await Book.find({});
-      return allBooks.length;
+      return Book.collection.countDocuments();
     },
     authorCount: async () => {
-      const allAuthors = await Author.find({});
-      return allAuthors.length;
+      return Author.collection.countDocuments();
     },
     allBooks: async (root, args) => {
-      const allBooks = await Book.find({});
-      return allBooks;
+      return Book.find({}).populate("author");
     },
-    allAuthors: () => {
-      return authors.map((author) => {
+    allAuthors: async () => {
+      const allBooks = await Book.find({}).populate("author").lean();
+      const allAuthors = await Author.find({}).lean();
+
+      const modifiedAuthors = allAuthors.map((author) => {
+        const bookCount = allBooks.reduce((count, book) => {
+          return book.author.name === author.name ? count + 1 : count;
+        }, 0);
+
         return {
           ...author,
-          bookCount: books.reduce(
-            (count, curBook) =>
-              curBook.author === author.name ? count + 1 : count,
-            0
-          ),
+          bookCount: bookCount,
         };
       });
+      return modifiedAuthors;
     },
   },
   Mutation: {
--- part8/ex8.1/populatedb.js
+++ part8/ex8.1/populatedb.js
@@ -11,22 +11,43 @@ mongoose
   .connect(MONGODB_URI)
   .then(() => {
     console.log("connected to MongoDB");
+    doPopulateDb().then("did the function!");
+    console.log("db populated");
   })
   .catch((error) => {
     console.log("error connection to MongoDB:", error.message);
   });
 
 const doPopulateDb = async () => {
-  const author = await Author.create({ name: "Daniel Scislowski", born: 1986 });
+  await Author.deleteMany({});
+  await Book.deleteMany({});
 
-  const book = await Book.create({
+  const author1 = await Author.create({
+    name: "Daniel Scislowski",
+    born: 1986,
+  });
+  const author2 = await Author.create({ name: "Blaise Pascal", born: 1610 });
+
+  const book1 = await Book.create({
     title: "broma",
-    author: author,
-    genres: ["fake"],
+    author: author1,
+    genres: ["fake", "nonsense"],
     published: 1977,
   });
 
-  console.log(book);
-};
+  const book2 = await Book.create({
+    title: "second title",
+    author: author1,
+    genres: ["fake", "drama"],
+    published: 2020,
+  });
 
-doPopulateDb().then("did the function!");
+  const book3 = await Book.create({
+    title: "Pensees",
+    author: author2,
+    genres: ["religion", "philosophy"],
+    published: 1660,
+  });
+
+  return true;
+};
