cc79f0b541a38521e3580f44f29507b01eae575d
Daniel Scislowski <mr.scislowski@gmail.com>
2023-08-02 20:33:06 -0500

    Exercise 10.7: scrollable app bar

--- part10/rate-repository-app/src/components/AppBar.jsx
+++ part10/rate-repository-app/src/components/AppBar.jsx
@@ -2,6 +2,7 @@ import Constants from "expo-constants";
 import theme from "../theme";
 import AppBarTab from "./AppBarTab";
 import styled from "styled-components/native";
+import { ScrollView } from "react-native";
 
 const Container = styled.View`
   padding-top: ${Constants.statusBarHeight}px;
@@ -12,12 +13,22 @@ const Container = styled.View`
   justify-content: space-around;
 `;
 
+// display: flex;
+//   flex-direction: row;
+//   justify-content: space-around;
+
 const AppBar = () => {
   return (
     <Container>
-      <AppBarTab title="REPOSITORIES" link="/" />
-      <AppBarTab title="SIGN IN" link="/SignIn" />
-      <AppBarTab title="ABOUT" link="/About" />
+      <ScrollView horizontal indicatorStyle="white">
+        <AppBarTab title="REPOSITORIES" link="/" />
+        <AppBarTab title="SIGN IN" link="/SignIn" />
+        <AppBarTab title="ABOUT" link="/About" />
+        <AppBarTab title="ABOUT2" link="/About2" />
+        <AppBarTab title="ABOUT3" link="/About3" />
+        <AppBarTab title="ABOUT4" link="/About4" />
+        <AppBarTab title="ABOUT5" link="/About5" />
+      </ScrollView>
     </Container>
   );
 };
--- part10/rate-repository-app/src/components/AppBarTab.jsx
+++ part10/rate-repository-app/src/components/AppBarTab.jsx
@@ -1,11 +1,19 @@
 import { Text } from "react-native";
 import { Link } from "react-router-native";
+import styled from "styled-components/native";
+
+const Container = styled.View`
+  padding-left: 5px;
+  padding-right: 5px;
+`;
 
 const AppBarTab = (props) => {
   return (
-    <Link to={props.link}>
-      <Text>{props.title}</Text>
-    </Link>
+    <Container>
+      <Link to={props.link}>
+        <Text>{props.title}</Text>
+      </Link>
+    </Container>
   );
 };
 
