2cf03cb227239c76384376433d920045d68c3ed9
Daniel Scislowski <mr.scislowski@gmail.com>
2023-08-04 18:45:14 -0500

    Exercise 10.9: validating the sign-in form

--- part10/rate-repository-app/src/components/SignInForm.jsx
+++ part10/rate-repository-app/src/components/SignInForm.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { TextInput, Text } from "react-native";
 import styled from "styled-components/native";
 import { useFormik } from "formik";
+import * as yup from "yup";
 
 const Container = styled.View``;
 
@@ -17,15 +18,43 @@ const FormFieldContainer = styled.View`
   margin: 5px;
   border-style: solid;
   border-width: 1px;
+  border-color: ${(props) => (props.error ? "red" : "black")};
+`;
+
+const FormFieldErrorMessage = styled.Text`
+  color: red;
 `;
 
 const FormField = (props) => {
+  const { formik, name, ...passThruProps } = props;
   return (
-    <FormFieldContainer>
-      <TextInput {...props} />
-    </FormFieldContainer>
+    <>
+      <FormFieldContainer error={formik.touched[name] && formik.errors[name]}>
+        <TextInput
+          value={formik.values[name]}
+          onChangeText={formik.handleChange(name)}
+          placeholder={props.name}
+          {...props}
+        />
+      </FormFieldContainer>
+      {formik.touched[name] && formik.errors[name] ? (
+        <FormFieldErrorMessage>{formik.errors[name]}</FormFieldErrorMessage>
+      ) : null}
+    </>
   );
 };
+
+const validationSchema = yup.object().shape({
+  username: yup
+    .string()
+    .min(4, "username must be 4 letters long or more")
+    .required("username is required"),
+
+  password: yup
+    .string()
+    .min(8, "password must be 8 letters or longer")
+    .required("password is required"),
+});
 
 const SignInForm = () => {
   const formik = useFormik({
@@ -38,22 +67,14 @@ const SignInForm = () => {
         `submitting username:${values.username}, password:${values.password}`
       );
     },
+    validationSchema: validationSchema,
   });
 
   return (
     <Container>
-      <FormField
-        value={formik.values.username}
-        onChangeText={formik.handleChange("username")}
-        placeholder="username"
-      />
+      <FormField formik={formik} name="username" />
 
-      <FormField
-        value={formik.values.password}
-        placeholder="password"
-        onChangeText={formik.handleChange("password")}
-        secureTextEntry
-      />
+      <FormField formik={formik} name="password" secureTextEntry />
 
       <SignInButton onPress={formik.handleSubmit}>
         <Text>Sign in</Text>
