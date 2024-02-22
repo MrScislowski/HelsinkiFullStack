e02526ae2003543fb5d0bd48f5961552f83f7f21
Daniel Scislowski <mr.scislowski@gmail.com>
2024-02-22 16:27:47 -0600

    1.7: unicafe step2

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -1,12 +1,22 @@
 const Statistics = (props) => {
   const { good, neutral, bad } = props;
 
+  const total = good + neutral + bad;
+  const avg = (good - bad) / total;
+  const percentPos = (good / total) * 100;
+
   return (
     <>
       <h2>Statistics</h2>
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
       <p>bad: {bad}</p>
+      <p>total votes: {total}</p>
+      <p>average feedback score: {total === 0 ? "" : avg.toFixed(2)}</p>
+      <p>
+        percent positive feedback:{" "}
+        {total === 0 ? "" : `${percentPos.toFixed(1)}%`}
+      </p>
     </>
   );
 };
