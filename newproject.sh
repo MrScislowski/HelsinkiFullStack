c79830b43b88c73c3f7548089b7c79d3f03608e8
Daniel Scislowski <mr.scislowski@gmail.com>
2024-02-22 18:03:14 -0600

    create blank phonebook app

---
+++ newproject.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+part="part2"
+appname="phonebook"
+
+cd $part
+pnpm create vite $appname --template react
+cd $appname
+rm -r public/ src/assets src/App.css src/index.css .eslintrc.cjs
+echo "" > README.md
+
