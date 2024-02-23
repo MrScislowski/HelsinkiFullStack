11438180547c30cb46a1b00dc66c7bc82ed4a095
Daniel Scislowski <mr.scislowski@gmail.com>
2024-02-22 18:27:30 -0600

    2.9*: The Phonebook Step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,14 +2,26 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "324-342" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <p>
+        filter shown with{" "}
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </p>
+      <h2>Add New</h2>
       <form
         onSubmit={(e) => {
           e.preventDefault();
@@ -17,7 +29,13 @@ const App = () => {
           if (persons.find((person) => person.name === newName)) {
             alert(`${newName} is already in the phonebook`);
           } else {
-            setPersons(persons.concat({ name: newName, number: newNumber }));
+            setPersons(
+              persons.concat({
+                name: newName,
+                number: newNumber,
+                id: persons.length + 1,
+              })
+            );
             setNewName("");
           }
         }}
@@ -38,11 +56,13 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <p key={person.name}>
-          {person.name} {person.number}
-        </p>
-      ))}
+      {persons
+        .filter((person) => person.name.includes(searchTerm))
+        .map((person) => (
+          <p key={person.id}>
+            {person.name} {person.number}
+          </p>
+        ))}
     </div>
   );
 };
