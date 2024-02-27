81dd38ea6c0d7400bf3bcc47e0554391eae85a2f
Daniel Scislowski <mr.scislowski@gmail.com>
2024-02-26 21:49:35 -0600

    implement filter

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -3,29 +3,31 @@ import axios from "axios";
 
 function App() {
   const [countrySearch, setCountrySearch] = useState("");
-  const [foundCountries, setFoundCountries] = useState("");
   const [countryDataList, setCountryDataList] = useState([]);
+
+  const filteredCountries = countryDataList.filter((c) =>
+    c.name.toLowerCase().includes(countrySearch.toLowerCase())
+  );
 
   useEffect(() => {
     axios
       .get("https://studies.cs.helsinki.fi/restcountries/api/all")
       .then((response) => {
-        setCountryDataList(
-          response.data.map((countryData) => {
-            return {
-              name: countryData["name"]["common"],
-              id: countryData["cca3"],
-              capital: countryData["capital"],
-              area: countryData["area"],
-              languages: !countryData["languages"]
-                ? []
-                : Object.keys(countryData["languages"]).map(
-                    (key) => countryData["languages"][key]
-                  ),
-              flagUrl: countryData["flags"]["png"],
-            };
-          })
-        );
+        const prunedData = response.data.map((countryData) => {
+          return {
+            name: countryData["name"]["common"],
+            id: countryData["cca3"],
+            capital: countryData["capital"],
+            area: countryData["area"],
+            languages: !countryData["languages"]
+              ? []
+              : Object.keys(countryData["languages"]).map(
+                  (key) => countryData["languages"][key]
+                ),
+            flagUrl: countryData["flags"]["png"],
+          };
+        });
+        setCountryDataList(prunedData);
       });
   }, []);
 
@@ -40,12 +42,15 @@ function App() {
       </p>
 
       <div>
-        Country names are:
-        <ul>
-          {countryDataList.map((cdata) => (
-            <li key={cdata.id}>{cdata.name}</li>
-          ))}
-        </ul>
+        {filteredCountries.length > 10 ? (
+          <p>too many countries; refine search</p>
+        ) : (
+          <ul>
+            {filteredCountries.map((cdata) => (
+              <li key={cdata.id}>{cdata.name}</li>
+            ))}
+          </ul>
+        )}
       </div>
     </>
   );
