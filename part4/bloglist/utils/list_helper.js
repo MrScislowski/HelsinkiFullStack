16e085bab54416a52ba034d803d321be79d1eac8
Daniel Scislowski <mr.scislowski@gmail.com>
2024-08-09 05:15:22 -0500

    4.6*: Helper Functions and Unit Tests, step 4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -16,6 +16,70 @@ const favoriteBlog = (blogs) => {
   return { title, author, likes }
 }
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+
+  const authorHistogram = {}
+  blogs.forEach(blog => {
+    if (Object.hasOwn(authorHistogram, blog.author)) {
+      authorHistogram[blog.author] += 1
+    } else {
+      authorHistogram[blog.author] = 1
+    }
+  })
+
+  const authors = Object.keys(authorHistogram)
+
+  const winner = authors.reduce((winningKey, blogKey) => {
+
+    if (authorHistogram[blogKey] > authorHistogram[winningKey]) {
+      return blogKey
+    } else {
+      return winningKey
+    }
+  }, authors[0])
+
+  return {
+    author: winner,
+    blogs: authorHistogram[winner]
+  }
+}
+
+// When I asked chatGPT to make this code more idiomatic it gave:
+// const mostBlogs = (blogs) => {
+//   if (blogs.length === 0) return null;
+
+//   const authorHistogram = blogs.reduce((acc, { author }) => {
+//     acc[author] = (acc[author] || 0) + 1;
+//     return acc;
+//   }, {});
+
+//   const [author, blogsCount] = Object.entries(authorHistogram)
+//     .reduce((max, entry) => entry[1] > max[1] ? entry : max);
+
+//   return { author, blogs: blogsCount };
+// }
+
+// and claude.ai gave:
+// const mostBlogs = (blogs) => {
+//   if (blogs.length === 0) return null;
+
+//   const authorHistogram = blogs.reduce((acc, blog) => {
+//     acc[blog.author] = (acc[blog.author] || 0) + 1;
+//     return acc;
+//   }, {});
+
+//   const [author, blogs] = Object.entries(authorHistogram).reduce(
+//     ([maxAuthor, maxBlogs], [author, blogs]) =>
+//       blogs > maxBlogs ? [author, blogs] : [maxAuthor, maxBlogs]
+//   );
+
+//   return { author, blogs };
+// };
+
+
 module.exports = {
-  dummy, totalLikes, favoriteBlog
+  dummy, totalLikes, favoriteBlog, mostBlogs
 }
