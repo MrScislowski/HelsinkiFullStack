febcae4f068a12666cfc039ef75414ab91ffc573
Daniel Scislowski <mr.scislowski@gmail.com>
2024-08-09 04:38:27 -0500

    4.5*: Helper Functions and Unit Tests, step 3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,6 +6,16 @@ const totalLikes = (blogs) => {
   return blogs.reduce((total, blog) => total + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+
+  const favorite =  blogs.reduce((favorite, curBlog) => curBlog.likes > favorite.likes ? curBlog : favorite, blogs[0])
+  const { title, author, likes } = favorite
+  return { title, author, likes }
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
