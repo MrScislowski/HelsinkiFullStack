bf19b64424c85b147ed0a91fcbfd55481d32ae68
Daniel Scislowski <mr.scislowski@gmail.com>
2024-08-08 19:06:28 -0500

    refactor to use logger util

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -3,6 +3,8 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 
+const logger = require('./utils/logger')
+
 require('dotenv').config()
 
 const Blog = require('./models/Blog')
@@ -33,5 +35,5 @@ app.post('/api/blogs', (request, response) => {
 
 const PORT = 3003
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
