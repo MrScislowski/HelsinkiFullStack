62763fa011c4ebfb164aae48341bb1479195a5b9
Daniel Scislowski <mr.scislowski@gmail.com>
2024-08-20 14:18:22 -0500

    5.14: Blog List Tests, step 2

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { expect } from 'vitest'
 import Blog from './Blog'
 
@@ -31,5 +32,36 @@ test('renders blog title and author, but not URL / number of likes', () => {
 
   const foundLikes = screen.queryByText(likesValue, { exact: false })
   expect(foundLikes).toBeNull()
+
+})
+
+test('URL and number of likes shown once button is clicked', async () => {
+  const titleText = 'abc'
+  const authorText = 'def'
+  const urlText = 'ghi'
+  const likesValue = 19
+  const userName = 'jkl'
+
+  const blog = {
+    id: '000',
+    user: { id: '000', username: '000', name: userName },
+    title: titleText,
+    author: authorText,
+    url: urlText,
+    likes: likesValue,
+  }
+
+  render(<Blog blog={blog} blogs={[]} setBlogs={() => false} setNotification={() => false} />)
+
+  const user = userEvent.setup()
+
+  const button = screen.getByRole('button', { name: /show/i } )
+  await user.click(button)
+
+  const foundUrl = screen.queryByText(urlText, { exact: false })
+  expect(foundUrl).toBeTruthy()
+
+  const foundLikes = screen.queryByText(likesValue, { exact: false })
+  expect(foundLikes).toBeTruthy()
 
 })
