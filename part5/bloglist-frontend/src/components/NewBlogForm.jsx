d602e07450412593c05c92a9b514c26db92a1310
Daniel Scislowski <mr.scislowski@gmail.com>
2024-08-21 11:13:50 -0500

    5.16: Blog List Tests, step 4

--- part5/bloglist-frontend/src/components/NewBlogForm.jsx
+++ part5/bloglist-frontend/src/components/NewBlogForm.jsx
@@ -2,7 +2,6 @@ import { useState, useRef } from 'react'
 import blogService from '../services/blogs'
 import Togglable from './Togglable'
 
-
 const NewBlogForm = ({ blogs, setBlogs, setNotification }) => {
   const [title, setTitle] = useState('')
   const [author, setAuthor] = useState('')
@@ -25,15 +24,9 @@ const NewBlogForm = ({ blogs, setBlogs, setNotification }) => {
       setNotification({
         message: 'added new blog',
         data: { title: response.title, author: response.author },
-        type: 'info'
+        type: 'info',
       })
       setTimeout(() => setNotification(null), 3000)
-
-      console.log('about to add the following response to the blog list...')
-      console.log(response)
-      console.log('the blogs already in the list are:')
-      console.log(blogs)
-
 
       setBlogs(blogs.concat(response).sort((a, b) => a.likes - b.likes))
     } catch (e) {
@@ -43,26 +36,35 @@ const NewBlogForm = ({ blogs, setBlogs, setNotification }) => {
 
   return (
     <>
-      <Togglable buttonLabel='new blog' ref={newBlogFormRef}>
+      <Togglable buttonLabel="new blog" ref={newBlogFormRef}>
         <h2>create new</h2>
         <form onSubmit={handleCreateNewBlog}>
-          <span>
-          title:
-            <input type='text' onChange={(e) => setTitle(e.target.value)} />
-          </span>
+          <label htmlFor="title">title:</label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            onChange={(e) => setTitle(e.target.value)}
+          />
           <br />
-          <span>
-          author:
-            <input type='text' onChange={(e) => setAuthor(e.target.value)} />
-          </span>
+          <label htmlFor="author">author:</label>
+          <input
+            type="text"
+            name="author"
+            id="author"
+            onChange={(e) => setAuthor(e.target.value)}
+          />
           <br />
-          <span>
-          url:
-            <input type='text' onChange={(e) => setUrl(e.target.value)} />
-          </span>
+          <label htmlFor="url">
+            url:
+          </label>
+            <input
+            type="text"
+            name="url"
+            id="url"
+            onChange={(e) => setUrl(e.target.value)} />
           <br />
-          <button type='submit'>create</button>
-
+          <button type="submit">create</button>
         </form>
       </Togglable>
     </>
