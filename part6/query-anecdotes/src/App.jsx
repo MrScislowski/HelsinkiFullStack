9bb8b5d096f35a8edc1e766f2ace1535ada266d3
Daniel Scislowski <mr.scislowski@gmail.com>
2024-08-26 23:51:06 -0500

    Exercise 6.22

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,12 +1,25 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import axios from 'axios'
 
 const App = () => {
+  const queryClient = useQueryClient()
+
+  const voteMutation = useMutation({
+    mutationFn: anecdote => {
+      return axios.patch(`http://localhost:3001/anecdotes/${anecdote.id}`, { ...anecdote, votes: anecdote.votes + 1}).then(res => res.data)
+    },
+    onSuccess: updatedAnecdote => {
+      const anecdotes = queryClient.getQueryData(['anecdotes'])
+      queryClient.setQueryData(['anecdotes'],
+        anecdotes.map(a => a.id !== updatedAnecdote.id ? a : updatedAnecdote)
+      )
+    }
+  })
 
   const handleVote = (anecdote) => {
-    console.log('vote')
+    voteMutation.mutate(anecdote)
   }
 
   const query = useQuery({
