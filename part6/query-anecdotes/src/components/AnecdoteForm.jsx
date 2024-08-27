830219f07bab3de73570f02e87d0b48cbbac5a4c
Daniel Scislowski <mr.scislowski@gmail.com>
2024-08-26 23:44:29 -0500

    Exercise 6.21

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,10 +1,25 @@
+import { useQueryClient, useMutation } from '@tanstack/react-query'
+import axios from 'axios'
+
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+
+  const newAnecdoteMutation = useMutation({
+    mutationFn: (content) => {
+      return axios.post('http://localhost:3001/anecdotes', { content, votes: 0 }).then(res => res.data)
+    },
+    onSuccess: (newAnecdote) => {
+      const anecdotes = queryClient.getQueryData(['anecdotes'])
+      queryClient.setQueryData(['anecdotes'], anecdotes.concat(newAnecdote))
+    }
+  })
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
+    newAnecdoteMutation.mutate(content)
     event.target.anecdote.value = ''
-    console.log('new anecdote')
 }
 
   return (
