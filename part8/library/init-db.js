d9b3c7cc7876a8772e2e1a96c16855637b6b07b0
Daniel Scislowski <mr.scislowski@gmail.com>
2024-09-07 05:07:49 -0500

    make db init script

---
+++ part8/library/init-db.js
@@ -0,0 +1,128 @@
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const Book = require("./models/Book");
+const Author = require("./models/Author");
+
+require("dotenv").config();
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+console.log("connecting to", MONGODB_URI);
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => console.log("connected to MongoDB"))
+  .catch((error) =>
+    console.log("error connecting to MongoDB: ", error.message)
+  );
+
+let authors = [
+  {
+    name: "Robert Martin",
+    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
+    born: 1952,
+  },
+  {
+    name: "Martin Fowler",
+    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
+    born: 1963,
+  },
+  {
+    name: "Fyodor Dostoevsky",
+    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
+    born: 1821,
+  },
+  {
+    name: "Joshua Kerievsky", // birthyear not known
+    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
+  },
+  {
+    name: "Sandi Metz", // birthyear not known
+    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
+  },
+];
+
+let books = [
+  {
+    title: "Clean Code",
+    published: 2008,
+    author: "Robert Martin",
+    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring"],
+  },
+  {
+    title: "Agile software development",
+    published: 2002,
+    author: "Robert Martin",
+    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
+    genres: ["agile", "patterns", "design"],
+  },
+  {
+    title: "Refactoring, edition 2",
+    published: 2018,
+    author: "Martin Fowler",
+    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring"],
+  },
+  {
+    title: "Refactoring to patterns",
+    published: 2008,
+    author: "Joshua Kerievsky",
+    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring", "patterns"],
+  },
+  {
+    title: "Practical Object-Oriented Design, An Agile Primer Using Ruby",
+    published: 2012,
+    author: "Sandi Metz",
+    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring", "design"],
+  },
+  {
+    title: "Crime and punishment",
+    published: 1866,
+    author: "Fyodor Dostoevsky",
+    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
+    genres: ["classic", "crime"],
+  },
+  {
+    title: "Demons",
+    published: 1872,
+    author: "Fyodor Dostoevsky",
+    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
+    genres: ["classic", "revolution"],
+  },
+];
+
+Book.deleteMany({})
+  .then(() => {
+    console.log("deleted books");
+    return Author.deleteMany({});
+  })
+  .then(() => {
+    console.log("deleted authors");
+    return Author.insertMany(
+      authors.map((a) => {
+        const { id, ...rest } = a;
+        return { ...rest };
+      })
+    );
+  })
+  .then((authorData) => {
+    console.log("added authors");
+    return Book.insertMany(
+      books.map((b) => {
+        const { id, author, ...rest } = b;
+        const authorId = authorData.find((a) => a.name === b.author)._id;
+        return { ...rest, author: authorId };
+      })
+    );
+  })
+  .finally(() => {
+    console.log("added books");
+    console.log("database populated");
+    mongoose.connection.close();
+  })
+  .catch((e) =>
+    console.log(`Error populating database: ${JSON.stringify(e, null, 2)}`)
+  );
