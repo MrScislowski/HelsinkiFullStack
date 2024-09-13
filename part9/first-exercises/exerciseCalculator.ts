157629349ecba42cee6684a4cf6ba855ea09fd19
Daniel Scislowski <mr.scislowski@gmail.com>
2024-09-12 21:29:27 -0500

    9.3 Command line

--- part9/first-exercises/exerciseCalculator.ts
+++ part9/first-exercises/exerciseCalculator.ts
@@ -1,3 +1,5 @@
+import { parseNumber } from "./utils";
+
 type Rating = 1 | 2 | 3;
 
 type RatingDescription = "not even close" | "almost there" | "you did it!";
@@ -12,7 +14,31 @@ interface Result {
   ratingDescription: RatingDescription;
 }
 
-const calculateExercises = (exerciseLog: number[], target: number): Result => {
+interface BmiCalculatorInput {
+  exerciseLog: number[];
+  target: number;
+}
+
+const parseBmiCalculatorInput = (args: string[]): BmiCalculatorInput => {
+  if (args.length < 4) {
+    throw new Error(
+      `Expected 2+ arguments (a target, then one or more training hours representing days). Receieved ${
+        args.length - 2
+      }`
+    );
+  }
+
+  const target = parseNumber(args[2]);
+  const exerciseLog = args.slice(3).map(parseNumber);
+  return {
+    target,
+    exerciseLog,
+  };
+};
+
+const calculateExercises = (data: BmiCalculatorInput): Result => {
+  const { exerciseLog, target } = data;
+
   const periodLength = exerciseLog.length;
   const trainingDays = exerciseLog.reduce(
     (total, hours) => (hours === 0 ? total : total + 1),
@@ -44,4 +70,13 @@ const calculateExercises = (exerciseLog: number[], target: number): Result => {
   };
 };
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+try {
+  const result = calculateExercises(parseBmiCalculatorInput(process.argv));
+  console.log(result);
+} catch (error: unknown) {
+  let message = "Error occurred\n";
+  if (error instanceof Error) {
+    message += error.message;
+  }
+  console.log(message);
+}
