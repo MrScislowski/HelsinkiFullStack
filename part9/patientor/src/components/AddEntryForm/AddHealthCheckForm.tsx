1c615ca7a71424b3346826065bd0bd2faeb99c8b
Daniel Scislowski <mr.scislowski@gmail.com>
2024-09-19 12:24:24 -0500

    wip - refactor AddHospitalForm and AddOccupationalHealthcareForm in the same way

--- part9/patientor/src/components/AddEntryForm/AddHealthCheckForm.tsx
+++ part9/patientor/src/components/AddEntryForm/AddHealthCheckForm.tsx
@@ -1,138 +1,25 @@
-import { useState } from "react";
-
-import { TextField, Grid, Button, Select, MenuItem } from "@mui/material";
-import patientsService from "../../services/patients";
-import { Diagnosis, Patient } from "../../types";
+import { TextField } from "@mui/material";
+import { EntrySansId, HealthCheckEntry } from "../../types";
 
 interface Props {
-  patient: Patient;
-  setPatient: React.Dispatch<React.SetStateAction<Patient | undefined>>;
-  diagnosisCodes: Diagnosis[];
+  formState: Omit<HealthCheckEntry, "id">;
+  setFormState: React.Dispatch<React.SetStateAction<EntrySansId>>;
 }
 
-const AddHealthCheckForm = ({
-  patient,
-  setPatient,
-  diagnosisCodes: allDiagnosisCodes,
-}: Props) => {
-  const [description, setDescription] = useState("");
-  const [date, setDate] = useState("");
-  const [specialist, setSpecialist] = useState("");
-  const [healthRating, setHealthRating] = useState("");
-  const [diagnosisCodes, setDiagnosisCodes] = useState<
-    Array<Diagnosis["code"]>
-  >([]);
-  const [notification, setNotification] = useState("");
-
-  const patientId = patient.id;
-
-  if (!patientId) return <p>patient id not found</p>;
-
-  const clearFormEntries = () => {
-    setDescription("");
-    setDate("");
-    setSpecialist("");
-    setHealthRating("");
-    setDiagnosisCodes([]);
-  };
-
-  const addEntry = (event: React.SyntheticEvent) => {
-    event.preventDefault();
-
-    patientsService
-      .addEntry(patientId, {
-        description,
-        date,
-        specialist,
-        healthCheckRating: Number(healthRating),
-        diagnosisCodes: diagnosisCodes,
-        type: "HealthCheck",
-      })
-      .then((newEntry) => {
-        clearFormEntries();
-        setPatient({ ...patient, entries: patient.entries.concat(newEntry) });
-      })
-      .catch((err: unknown) => {
-        if (err instanceof Error) {
-          setNotification(err.message);
-        } else {
-          setNotification("unknown error. Check console.");
-          console.log(err);
-        }
-        setTimeout(() => setNotification(""), 5000);
-      });
-  };
-
-  const Notification = () => {
-    if (notification === "") return null;
-
-    return <p style={{ color: "red" }}>{notification}</p>;
-  };
-
+const AddHealthCheckForm = ({ formState, setFormState }: Props) => {
   return (
     <>
-      <h3>New HealthCheck Entry</h3>
-      <Notification />
-      <form onSubmit={addEntry}>
-        <TextField
-          label="Description"
-          fullWidth
-          value={description}
-          onChange={({ target }) => setDescription(target.value)}
-        />
-        <TextField
-          label="Date"
-          fullWidth
-          value={date}
-          type="date"
-          onChange={({ target }) => setDate(target.value)}
-        />
-        <TextField
-          label="Specialist"
-          fullWidth
-          value={specialist}
-          onChange={({ target }) => setSpecialist(target.value)}
-        />
-        <TextField
-          label="Healthcheck rating"
-          fullWidth
-          value={healthRating}
-          onChange={({ target }) => setHealthRating(target.value)}
-        />
-
-        <Select
-          label="Diagnosis codes"
-          fullWidth
-          multiple
-          value={diagnosisCodes}
-          onChange={(e) => {
-            setDiagnosisCodes(diagnosisCodes.concat(e.target.value));
-          }}
-        >
-          {allDiagnosisCodes.map((code) => {
-            return (
-              <MenuItem
-                key={code.code}
-                value={code.code}
-              >{`${code.code} - ${code.name}`}</MenuItem>
-            );
-          })}
-        </Select>
-
-        <Grid style={{ display: "flex", justifyContent: "space-between" }}>
-          <Button
-            color="secondary"
-            variant="contained"
-            type="button"
-            onClick={clearFormEntries}
-          >
-            Cancel
-          </Button>
-          <Button type="submit" variant="contained">
-            Add
-          </Button>
-        </Grid>
-      </form>
+      <TextField
+        label="Healthcheck rating"
+        fullWidth
+        value={formState.healthCheckRating}
+        onChange={({ target }) =>
+          setFormState({
+            ...formState,
+            healthCheckRating: Number(target.value),
+          })
+        }
+      />
     </>
   );
 };
--- part9/patientor/src/components/AddEntryForm/index.tsx
+++ part9/patientor/src/components/AddEntryForm/index.tsx
@@ -1,15 +1,20 @@
-import {
-  useEffect,
-  useState,
-} from "react";
+import { useEffect, useState } from "react";
 
-import { Diagnosis, Patient } from "../../types";
+import {
+  blankHealthCheckEntry,
+  blankHospitalEntry,
+  blankOccupationalHealtcareEntry,
+  Diagnosis,
+  EntrySansId,
+  Patient,
+} from "../../types";
 import AddHealthCheckForm from "./AddHealthCheckForm";
 import AddHospitalForm from "./AddHospitalForm";
 
-import { Button } from "@mui/material";
+import { Button, Grid, MenuItem, Select, TextField } from "@mui/material";
 import AddOccupationalHealthcareForm from "./AddOccupationalHealthcareForm";
 import diagnoses from "../../services/diagnoses";
+import patientsService from "../../services/patients";
 
 interface Props {
   patient: Patient;
@@ -18,24 +23,176 @@ interface Props {
 
 const AddEntryForm = ({ patient, setPatient }: Props) => {
   const [entryType, setEntryType] = useState("Health Check");
-  const [diagnosisCodes, setDiagnosisCodes] = useState<Diagnosis[]>([]);
+  const [allDiagnosisCodes, setAllDiagnosisCodes] = useState<Diagnosis[]>([]);
+
+  const [formState, setFormState] = useState<EntrySansId>(
+    entryType === "Health Check"
+      ? blankHealthCheckEntry
+      : entryType === "Hospital"
+      ? blankHospitalEntry
+      : blankOccupationalHealtcareEntry
+  );
+
+  const [notification, setNotification] = useState("");
 
   useEffect(() => {
-    diagnoses.getAll().then(result => setDiagnosisCodes(result))
-  }, [])
+    diagnoses.getAll().then((result) => setAllDiagnosisCodes(result));
+  }, []);
+
+  const patientId = patient.id;
+
+  const clearFormEntries = () => {
+    setFormState(
+      entryType === "Health Check"
+        ? blankHealthCheckEntry
+        : entryType === "Hospital"
+        ? blankHospitalEntry
+        : blankOccupationalHealtcareEntry
+    );
+  };
+
+  const addEntry = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+
+    patientsService
+      .addEntry(patientId, formState)
+      .then((newEntry) => {
+        clearFormEntries();
+        setPatient({ ...patient, entries: patient.entries.concat(newEntry) });
+      })
+      .catch((err: unknown) => {
+        if (err instanceof Error) {
+          setNotification(err.message);
+        } else {
+          setNotification("unknown error. Check console.");
+          console.log(err);
+        }
+        setTimeout(() => setNotification(""), 5000);
+      });
+  };
+
+  const Notification = () => {
+    if (notification === "") return null;
+
+    return <p style={{ color: "red" }}>{notification}</p>;
+  };
+
+  if (!patientId) return <p>patient id not found</p>;
 
   return (
     <div style={{ borderStyle: "dotted", padding: "10px" }}>
-      <div style={{display: "flex", justifyContent: "space-evenly"}}>
-      <Button style={{fontWeight: entryType==="Health Check" ? "bold" : "normal"}} onClick={() => setEntryType("Health Check")}>Health Check</Button>
-      <Button style={{fontWeight: entryType==="Hospital" ? "bold" : "normal"}} onClick={() => setEntryType("Hospital")}>Hospital</Button>
-      <Button style={{fontWeight: entryType==="Occupational Healthcare" ? "bold" : "normal"}} onClick={() => setEntryType("Occupational Healthcare")}>Occupational Healthcare</Button>
+      <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+        <Button
+          style={{
+            fontWeight: entryType === "Health Check" ? "bold" : "normal",
+          }}
+          onClick={() => {
+            setEntryType("Health Check");
+            setFormState(blankHealthCheckEntry);
+          }}
+        >
+          Health Check
+        </Button>
+        <Button
+          style={{ fontWeight: entryType === "Hospital" ? "bold" : "normal" }}
+          onClick={() => {
+            setEntryType("Hospital");
+            setFormState(blankHospitalEntry);
+          }}
+        >
+          Hospital
+        </Button>
+        <Button
+          style={{
+            fontWeight:
+              entryType === "Occupational Healthcare" ? "bold" : "normal",
+          }}
+          onClick={() => {
+            setEntryType("Occupational Healthcare");
+            setFormState(blankOccupationalHealtcareEntry);
+          }}
+        >
+          Occupational Healthcare
+        </Button>
       </div>
 
-      {entryType === "Health Check" && <AddHealthCheckForm patient={patient} setPatient={setPatient} diagnosisCodes={diagnosisCodes} />}
-      {entryType === "Hospital" && <AddHospitalForm patient={patient} setPatient={setPatient} diagnosisCodes={diagnosisCodes} />}
-      {entryType === "Occupational Healthcare" && <AddOccupationalHealthcareForm patient={patient} setPatient={setPatient} diagnosisCodes={diagnosisCodes} />}
+      <Notification />
 
+      <form onSubmit={addEntry}>
+        {/* BASE FORM ELEMENTS */}
+        <TextField
+          label="Date"
+          fullWidth
+          value={formState.date}
+          type="date"
+          onChange={({ target }) =>
+            setFormState({ ...formState, date: target.value })
+          }
+        />
+        <TextField
+          label="Description"
+          fullWidth
+          value={formState.description}
+          onChange={({ target }) =>
+            setFormState({ ...formState, description: target.value })
+          }
+        />
+        <TextField
+          label="Specialist"
+          fullWidth
+          value={formState.specialist}
+          onChange={({ target }) =>
+            setFormState({ ...formState, specialist: target.value })
+          }
+        />
+
+        <Select
+          label="Diagnosis codes"
+          fullWidth
+          multiple
+          value={formState.diagnosisCodes}
+          onChange={(e) => {
+            setFormState({
+              ...formState,
+              diagnosisCodes: formState.diagnosisCodes?.concat(e.target.value),
+            });
+          }}
+        >
+          {allDiagnosisCodes.map((code) => {
+            return (
+              <MenuItem
+                key={code.code}
+                value={code.code}
+              >{`${code.code} - ${code.name}`}</MenuItem>
+            );
+          })}
+        </Select>
+
+        {formState.type === "HealthCheck" && (
+          <AddHealthCheckForm
+            formState={formState}
+            setFormState={setFormState}
+          />
+        )}
+        {/* {entryType === "Hospital" && <AddHospitalForm />}
+        {entryType === "Occupational Healthcare" && (
+          <AddOccupationalHealthcareForm />
+        )} */}
+
+        <Grid style={{ display: "flex", justifyContent: "space-between" }}>
+          <Button
+            color="secondary"
+            variant="contained"
+            type="button"
+            onClick={clearFormEntries}
+          >
+            Cancel
+          </Button>
+          <Button type="submit" variant="contained">
+            Add
+          </Button>
+        </Grid>
+      </form>
     </div>
   );
 };
