cfb21d277f5e191924b0fdb5582524bf128df7e1
Daniel Scislowski <mr.scislowski@gmail.com>
2024-10-25 15:31:25 -0500

    Exercise 13.1.

---
+++ part13/app/index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const { Sequelize, QueryTypes } = require("sequelize");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const sequelize = new Sequelize(process.env.DATABASE_URL);
+
+const notes = sequelize
+  .query("SELECT * FROM notes", QueryTypes.SELECT)
+  .then((data) => {
+    console.log("data received is:");
+    console.log(JSON.stringify(data, null, 2));
+  });
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
