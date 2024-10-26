bcddc2b15b2323a9338268a43bd4b686109b24e6
Daniel Scislowski <mr.scislowski@gmail.com>
2024-10-26 17:53:14 -0500

    implement GET /api/blogs

--- part13/app/index.js
+++ part13/app/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const { Sequelize, QueryTypes } = require("sequelize");
 const express = require("express");
 const cors = require("cors");
+const BlogInit = require("./models/BlogInit");
 
 const app = express();
 
@@ -9,13 +10,13 @@ app.use(express.json());
 app.use(cors());
 
 const sequelize = new Sequelize(process.env.DATABASE_URL);
+const Blog = BlogInit(sequelize);
+Blog.sync();
 
-const notes = sequelize
-  .query("SELECT * FROM blogs", QueryTypes.SELECT)
-  .then((data) => {
-    console.log("data received is:");
-    console.log(JSON.stringify(data, null, 2));
-  });
+app.get("/api/blogs", async (req, res) => {
+  const blogs = await Blog.findAll();
+  res.json(blogs);
+});
 
 app.listen(3000, () => {
   console.log("listening on port 3000");
