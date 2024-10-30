7ef61f6abb30691d1954b0c4c016acb126ffb5c1
Daniel Scislowski <mr.scislowski@gmail.com>
2024-10-30 17:48:22 -0500

    Exercise 13.22.

--- part13/app/routes/readingList.js
+++ part13/app/routes/readingList.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const { getUserFromToken } = require("../middleware/user");
 
 const { ReadingList } = require("../models/index");
 
+router.get("/", async (req, res) => {
+  const response = await ReadingList.findAll();
+  res.json(response);
+});
+
 router.post("/", async (req, res) => {
   const response = await ReadingList.create(req.body);
+  res.json(response);
+});
+
+router.put("/:id", getUserFromToken, async (req, res) => {
+  const entry = await ReadingList.findByPk(req.params.id);
+
+  if (req.user.id !== entry.userId) {
+    throw new Error("you can only mark your own blogs as read");
+  }
+
+  entry.read = req.body.read;
+
+  const response = await entry.save();
   res.json(response);
 });
 
