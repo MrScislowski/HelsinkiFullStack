d9ad6e836710df7cc2a46e9dbf69733b2bebbd92
Daniel Scislowski <mr.scislowski@gmail.com>
2024-10-31 13:57:10 -0500

    check session in middleware

--- part13/app/middleware/user.js
+++ part13/app/middleware/user.js
@@ -1,18 +1,31 @@
 const jwt = require("jsonwebtoken");
 const config = require("../utils/config");
+const Session = require("../models/Session");
 
 const getUserFromToken = async (req, res, next) => {
+  let userFromToken;
   if (
     req.headers &&
     req.headers.authorization &&
     req.headers.authorization.match("^[Bb]earer ")
   ) {
     const token = req.headers.authorization.substring(7);
-    const userFromToken = jwt.verify(token, config.SECRET);
+    userFromToken = jwt.verify(token, config.SECRET);
+
     req.user = userFromToken;
   } else {
     throw new Error("Must be logged in and provide token to make a post");
   }
+
+  const result = await Session.findAndCountAll({
+    userId: userFromToken.id,
+    sessionId: userFromToken.sessionId,
+  });
+
+  if (result.count !== 1) {
+    throw new Error("Session invalid");
+  }
+
   next();
 };
 
